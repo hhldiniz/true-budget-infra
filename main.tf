a4bf3bb93edcc5fa3e3c23763adfef0fab09ede6
Hugo <hhldiniz@gmail.com>
2022-08-22 20:06:30 -0300

    Fix cloud block location

--- main.tf
+++ main.tf
@@ -5,18 +5,17 @@ terraform {
       version = "~> 5.0"
     }
   }
-}
-
-cloud {
+  cloud {
     organization = "hugodiniz"
 
     workspaces {
       name = "true-budget-infra"
     }
   }
+}
 
 provider "heroku" {
-  email = var.heroku_username
+  email   = var.heroku_username
   api_key = var.heroku_api_key
 }
 
@@ -34,6 +33,6 @@ resource "heroku_app" "true_budget" {
 
 resource "heroku_addon" "database" {
   app_id = heroku_app.true_budget.id
-  plan = "heroku-postgresql:hobby-dev"
-  name = var.database_name
+  plan   = "heroku-postgresql:hobby-dev"
+  name   = var.database_name
 }
