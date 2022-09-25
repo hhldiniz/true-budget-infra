4ab6a7212d249deff1421233dd1e883d0ddebd44
Hugo <hhldiniz@gmail.com>
2022-09-25 13:15:52 -0300

    Changing database to a flexible sql database

--- database.tf
+++ database.tf
@@ -1,20 +1,10 @@
-resource "azurerm_postgresql_server" "database_server" {
+resource "azurerm_postgresql_flexible_server" "database_server" {
   name                         = "${var.app_name}-database-server"
-  sku_name                     = "S0"
+  sku_name                     = "B_Standard_B1ms"
   location                     = var.resource_group_location
   resource_group_name          = local.resource_group_name
-  storage_mb                   = 5120
-  auto_grow_enabled            = false
+  storage_mb                   = 32768
   administrator_login          = var.database_admin_user
-  administrator_login_password = var.database_admin_password
-  version                      = "11"
-  ssl_enforcement_enabled = true
-}
-
-resource "azurerm_postgresql_database" "database" {
-  name                = var.database_name
-  server_name         = azurerm_postgresql_server.database_server.name
-  resource_group_name = local.resource_group_name
-  charset             = "UTF8"
-  collation           = "English_United States.1252"
+  administrator_password = var.database_admin_password
+  version                      = "12"
 }
